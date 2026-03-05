12b1515f5378ca9d9ff571877c7db019ee416e08
vahid sk <vahidvadu098@gmail.com>
2026-03-05 14:40:53 +0530

    add project files

--- routes/list.js
+++ routes/list.js
@@ -5,11 +5,13 @@ const { wrapAsync } = require('../utils/wrapAsync');
 const ExpressError = require('../utils/ExpressError');
 
 const { listingSchema } = require('../joiSchema')
+const Listing = require('../models/listing');
 const { isLoggedIn, isOwnerForListing } = require('../middlewares/auth')
 const { getAllList, getSingleList, createListForm, createList, editList, updateList, deleteList } = require('../controllers/listingController')
 const multer = require('multer')
 const { storage } = require('../cloudConfig')
-const upload = multer({ storage, limits: { fileSize: 4 * 1024 * 1024 } // 4MB
+const upload = multer({
+    storage, limits: { fileSize: 4 * 1024 * 1024 } // 4MB
 });
 
 // ================= VALIDATION =================
@@ -46,5 +48,29 @@ router.put('/edit/list/:id', isLoggedIn, isOwnerForListing, upload.single('image
 router.delete('/deletelist/:id', isLoggedIn, isOwnerForListing, wrapAsync(deleteList));
 
 
+//search list
+router.get("/search", wrapAsync(async (req, res) => {
+    const { query } = req.query;
+
+    if (!query || query.trim() === "") {
+        req.flash("error", "Please enter something to search");
+        return res.redirect("/listings");
+    }
+
+    const lists = await Listing.find({
+        $or: [
+            { title: { $regex: query, $options: "i" } },
+            { location: { $regex: query, $options: "i" } },
+            { country: { $regex: query, $options: "i" } }
+        ]
+    });
+
+    if (lists.length === 0) {
+        req.flash("error", "No listing found for this search");
+        return res.redirect("/listings");
+    }
+
+    res.render("listings/index", { lists });
+}));
 
 module.exports = router;
